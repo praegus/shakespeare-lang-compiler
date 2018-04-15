9899930705ce4b7100949a33f2fe43e632bba6f1
goolm1 <maarten-jan.vangool@kadaster.nl>
2018-04-15 16:38:29 +0200

    interpreter van primes afgemaakt. extra line types toegevoegd; en interpretatie van act en scene verbeterd. Nu fixen dat er een spatie in characters kan zitten.

--- src/main/java/nl/java/shakespearelang/parser/Scene.java
+++ src/main/java/nl/java/shakespearelang/parser/Scene.java
@@ -2,14 +2,18 @@ package nl.java.shakespearelang.parser;
 
 import lombok.Getter;
 import nl.java.shakespearelang.parser.line.Assignment;
+import nl.java.shakespearelang.parser.line.Conditional;
 import nl.java.shakespearelang.parser.line.Enter;
 import nl.java.shakespearelang.parser.line.Exit;
+import nl.java.shakespearelang.parser.line.Goto;
 import nl.java.shakespearelang.parser.line.InputStatement;
 import nl.java.shakespearelang.parser.line.Line;
 import nl.java.shakespearelang.parser.line.OutputStatement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static nl.java.shakespearelang.parser.RomanToArabicConverter.romanToArabic;
 
@@ -19,56 +23,75 @@ public class Scene {
     private int sceneNumber;
     private List<Line> lines = new ArrayList<>();
 
-    public Scene(String titleAndScene, int sceneNumber) {
-        this.sceneNumber = sceneNumber;
-        if (!titleAndScene.contains(".")) {
+    public Scene(String sceneString, int number) {
+        if (!sceneString.contains(".")) {
             throw new RuntimeException("Title of scene is not ended properly with a dot!");
         }
-        setTitleAndsceneNumber(titleAndScene.substring(0, titleAndScene.indexOf(".")).trim());
-
-        String content = titleAndScene.substring(titleAndScene.indexOf(".") + 1).trim();
-        setLines(content);
+        String[] titleAndLines = sceneString.split("\\.");
+        this.sceneNumber = checkSceneNumber(number, titleAndLines[0]);
+        this.title = extractTitle(titleAndLines[0]);
+        addLines(titleAndLines);
     }
 
-    private void setTitleAndsceneNumber(String titleRaw) {
-        if (!titleRaw.contains(":")) {
-            throw new RuntimeException("Title of scene does not contain an act number with a semicolumn!");
+    private int checkSceneNumber(int number, String titleRaw) {
+        if(!titleRaw.contains("scene ") || !titleRaw.contains(":")){
+            throw new RuntimeException("Title of scene does not contain 'scene' or a semicolumn!");
         }
-        String romanNumeral = titleRaw.substring(0, titleRaw.indexOf(":"));
-        if (romanToArabic(romanNumeral) != sceneNumber) {
+        String romanNumeral = titleRaw.substring(0, titleRaw.indexOf(":")).replace("scene", "").trim();
+        if (romanToArabic(romanNumeral) != number) {
             throw new RuntimeException("Scene numbering is not in sequence!");
         }
-        this.title = titleRaw.substring(titleRaw.indexOf(":") + 1).trim();
+        return number;
     }
 
-    private void setLines(String content) {
-        String[] linesString = content.split("\\.");
+    private String extractTitle(String title) {
+        Pattern pattern = Pattern.compile("scene\\s\\w.*:(.*?)");
+        Matcher matcher = pattern.matcher(title);
+        if (matcher.find()) {
+            return matcher.group(1).trim();
+        }
+        throw new RuntimeException("Not title in scene found!");
+    }
 
+    private void addLines(String[] titleAndLines) {
         String currentSubject = "";
-        for (String line : linesString) {
+        for (int i = 1; i < titleAndLines.length; i++) {
+            String line = titleAndLines[i];
             if (line.contains("enter")) {
                 lines.add(new Enter(line));
             } else if (line.contains("exit") || line.contains("exeunt")) {
                 lines.add(new Exit(line));
             } else if (line.contains(":")) {
                 currentSubject = line.substring(0, line.indexOf(":"));
-                addLine(currentSubject, line.substring(line.indexOf(":") + 1).trim());
+                addLineHelper(currentSubject, line.substring(line.indexOf(":") + 1).trim());
             } else {
-                addLine(currentSubject, line);
+                addLineHelper(currentSubject, line);
             }
         }
     }
 
-    private void addLine(String currentSubject, String line) {
+    private void addLineHelper(String currentSubject, String line) {
         line = line.trim();
         if (line.equals("listen to your heart")) {
             lines.add(new InputStatement(currentSubject, line));
         } else if (line.equals("speak your mind") || line.equals("speak thy mind")) {
             lines.add(new OutputStatement(currentSubject, line, false));
+        } else if (line.equals("open your heart")) {
+            lines.add(new OutputStatement(currentSubject, line, true));
         } else if (line.startsWith("you")) {
             lines.add(new Assignment(currentSubject, line));
         } else if (line.startsWith("thou")) {
             lines.add(new Assignment(currentSubject, line));
+        } else if (line.startsWith("art thou")) {
+            lines.add(new Conditional(currentSubject, line));
+        } else if (line.startsWith("am i")) {
+            lines.add(new Conditional(currentSubject, line));
+        } else if (line.startsWith("is the")) {
+            lines.add(new Conditional(currentSubject, line));
+        } else if (line.startsWith("if so")) {
+            lines.add(new Goto(currentSubject, line));
+        } else if (line.startsWith("let us")) {
+            lines.add(new Goto(currentSubject, line));
         } else {
             throw new RuntimeException("type of line is unclear!");
         }
---
+++ src/main/java/nl/java/shakespearelang/parser/line/Conditional.java
@@ -0,0 +1,7 @@
+package nl.java.shakespearelang.parser.line;
+
+public class Conditional extends Line {
+    public Conditional(String subject, String line) {
+        super(subject, line);
+    }
+}
